353b7aef0dae1193aa364b78a767995cba205528
ehsan2793 <ehsan2793@gmail.com>
2022-04-17 11:46:10 -0700

    app starts

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <ul>
+    <li></li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 1,
+          title: 'Offical guide',
+          description: 'The offical Vue.js documentation ',
+          link: 'http://vue.js.org',
+        },
+        {
+          id: 2,
+          title: 'Google search',
+          description: 'Leanr to google',
+          link: 'http://google.com',
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style></style>
